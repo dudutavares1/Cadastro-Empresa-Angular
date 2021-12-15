bf674ba35bf3fc939f5dc166d2a444489ed5f54e
Dudu Tavares <81688169+dudutavares1@users.noreply.github.com>
2021-12-15 20:20:09 -0300

    tirando banner de menu
    tirando banner de menu

--- frontend/src/app/components/template/header/header.service.ts
+++ frontend/src/app/components/template/header/header.service.ts
@@ -7,8 +7,8 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class HeaderService {
 	private _headerData = new BehaviorSubject<HeaderData>({
-		title: 'Início',
-		icon: 'home',
+		title: '',
+		icon: '',
 		routeUrl: '',
 	});
 
